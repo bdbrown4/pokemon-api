39d1c865c51a26374ea75aa7563ffb531f875004
Ben Brown <bdbrown4@eagles.usi.edu>
2020-03-29 15:04:04 -0400

    updating mongoose

--- services/mongoose-provider.service.js
+++ services/mongoose-provider.service.js
@@ -1,7 +1,7 @@
 // Import Mongoose
 const mongoose = require('mongoose');
 // Connect to Mongoose and set connection variable
-mongoose.connect('mongodb://localhost:27017/PokemonStorageCenter', { useNewUrlParser: true});
+mongoose.connect('mongodb://localhost:27017/PokemonStorageCenter', { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.set('useCreateIndex', true);
 let mongooseDbConnection = mongoose.connection;
 
